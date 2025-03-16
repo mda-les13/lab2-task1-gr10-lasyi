e46dc71e462365d3c5d4995c5365cc01c207dbb6
mda-les13 <145479039+mda-les13@users.noreply.github.com>
2025-03-16 07:18:09 +0000

    Add function has_unique_digits

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -1,5 +1,21 @@
 /* hello.c */
+#include <stdbool.h>
 #include <stdio.h>
+
+bool has_unique_digits(int num) {
+  int digits[10] = {0};
+
+  while (num > 0) {
+    int digit = num % 10;
+    if (digits[digit] > 0) {
+      return false;
+    }
+    digits[digit]++;
+    num /= 10;
+  }
+
+  return true;
+}
 
 void main(void) {
   printf("Hello World\n");
